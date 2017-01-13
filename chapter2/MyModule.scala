e74eef3337255661d49e1747a2208a7c92200f0f
qiaoda.zqd <qiaoda.zqd@alibaba-inc.com>
2017-01-13 22:13:15 +0800

    add formatFacorial

--- chapter2/MyModule.scala
+++ chapter2/MyModule.scala
@@ -4,12 +4,28 @@ object MyModule {
 		if (n < 0) -n
 		else n
 
+	def factorial(n : Int) : Int = {
+		@annotation.tailrec
+		def go (n: Int, acc: Int): Int = {
+			if (n <= 0) acc
+			else go(n-1, n*acc)
+		}
+		
+		go(n, 1)
+	}
+
 	private def formatAbs(x : Int) = {
 		val msg = "The absolute value of %d is %d"
 		msg.format(x, abs(x))
 	}
 
+	private def formatFacorial(n : Int) = {
+		val msg = "The factorial value of %d is %d"
+		msg.format(n, factorial(n))
+	}
+
 	def main(args: Array[String]): Unit = {
 		println(formatAbs(-42))
+		println(formatFacorial(7))
 	}
 }
