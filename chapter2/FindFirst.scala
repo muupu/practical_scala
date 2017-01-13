340bb72d462ef5effc23546992c32bd35ceaf723
muupu <once645@gmail.com>
2017-01-13 22:34:46 +0800

    add findFirst[A] method

--- chapter2/FindFirst.scala
+++ chapter2/FindFirst.scala
@@ -12,8 +12,20 @@ object FindFirst {
 		loop(0)
 	}
 
+	def findFirst[A](ss : Array[A], p: A => Boolean) : Int = {
+		@annotation.tailrec
+		def loop (n: Int): Int = {
+			if (n >= ss.length) -1
+			else if (p(ss(n))) n
+			else loop(n + 1)
+		}
+		
+		loop(0)
+	}
+
 	def main(args: Array[String]): Unit = {
 		var list = Array("AAA", "BBB", "CCC", "DDD")
 		println(findFirst(list, "CCC"))
+		println(findFirst(Array(7, 9, 13), (x: Int) => x == 9))
 	}
 }
